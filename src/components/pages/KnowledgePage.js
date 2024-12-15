8d5f5a8bf123d3a4b07356dba0e0ec915d95f4fa
DENG <dyccc01@gmail.com>
2024-12-15 23:41:23 +0800

    update Knowledge

--- src/components/pages/KnowledgePage.js
+++ src/components/pages/KnowledgePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import "./KnowledgePage.css"; // 可選：新增樣式
 
 const KnowledgePage = () => {
   return (
@@ -15,10 +14,6 @@ const KnowledgePage = () => {
       <div className="knowledge-page knowledge-content noto-sans-sc">
         <h1>...</h1>
         <p>...</p>
-        <ul>
-          <li>...</li>
-          <li>...</li>
-        </ul>
       </div>
     </>
   );
